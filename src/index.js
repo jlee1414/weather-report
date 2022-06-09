a86bade3bca34023d6fdfac8a3502bfc2aa3b0db
jlee1414 <junniejlee@gmail.com>
2022-06-08 18:22:13 -0700

    Finished wave3, created function registerEventHandlers and if statement

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-"use strict";
+'use strict';
+
+/* wave 2 
+--> element that displays temp
+--> 2 clickable elements: 
+    +1 degree on click and 
+    -1 degree on click 
+--> element that displays landscape
+*/
+const increaseTemp = () => {
+  state.tempCount +=1;
+};
+
+const decreaseTemp = () => {};
+
+// wave 3, city name must update every time there is a text input
+
+const getCityInput = () => {
+  let inputValue = document.getElementById('cityTemp').value;
+  const city = document.getElementById('cityName');
+  city.textContent = inputValue;
+};
+const registerEventHandlers = () => {
+  const cityName = document.getElementById('cityTemp');
+  cityName.addEventListener('change', getValueInput);
+}
+
+if (document.readyState !== 'loading') {
+  registerEventHandlers();
+} else {
+  document.addEventListener('DOMContentLoaded', registerEventHandlers);
+}
